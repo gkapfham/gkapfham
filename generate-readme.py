5adf262e28f1c6989efeb83712c18dad52a7d0b6
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-03-01 19:17:32 -0500

    feat: Add all of the link generation to the generate-readme.py script.

--- generate-readme.py
+++ generate-readme.py
@@ -6,29 +6,51 @@ from rich.tree import Tree
 
 console = Console(record=True, width=100)
 
+# teaching links
 teaching_tree = Tree(
     "🎉 Teaching",
     guide_style="bold red",
 )
-teaching_tree.add("[link=https://algorithmology.org/]Algorithm Analysis[/link]", guide_style="green")
-teaching_tree.add("[link=https://securitysynapse.org/]Computer Security[/link]", guide_style="green")
-teaching_tree.add("[link=https://developerdevelopment.com/]Software Engineering[/link]", guide_style="green")
+teaching_tree.add("[link=https://algorithmology.org/]Algorithm Analysis[/link]")
+teaching_tree.add("[link=https://securitysynapse.org/]Computer Security[/link]")
+teaching_tree.add("[link=https://developerdevelopment.com/]Software Engineering[/link]")
 
+# research links
 research_tree = Tree(
     "🔬 Research",
     guide_style="bold red",
 )
-research_tree.add("[link=https://www.gregorykapfhammer.com/research/papers/#category=test-suite%20prioritization]Regression Testing[/link]", guide_style="green")
-research_tree.add("[link=https://www.gregorykapfhammer.com/research/papers/#category=mutation%20testing]Mutation Testing[/link]", guide_style="green")
-research_tree.add("[link=https://www.gregorykapfhammer.com/research/papers/#category=flaky%20tests]Flaky Tests[/link]", guide_style="green")
+research_tree.add(
+    "[link=https://www.gregorykapfhammer.com/research/papers/#category=test-suite%20prioritization]Regression Testing[/link]"
+)
+research_tree.add(
+    "[link=https://www.gregorykapfhammer.com/research/papers/#category=mutation%20testing]Mutation Testing[/link]"
+)
+research_tree.add(
+    "[link=https://www.gregorykapfhammer.com/research/papers/#category=flaky%20tests]Flaky Tests[/link]"
+)
 
 software_tree = Tree(
     "🛠️ Software",
     guide_style="bold red",
 )
-software_tree.add("GatorGrader", guide_style="green")
-software_tree.add("Cellveyor", guide_style="green")
-software_tree.add("RepoRover", guide_style="green")
+software_tree.add("[link=https://github.com/AstuteSource/chasten]Chasten[/link]")
+software_tree.add("[link=https://github.com/GatorEducator/gatorgrade]GatorGrade[/link]")
+software_tree.add("[link=https://github.com/GatorEducator/reporover]RepoRover[/link]")
+
+podcast_tree = Tree(
+    "🎤 Podcasts",
+    guide_style="bold red",
+)
+podcast_tree.add(
+    "[link=https://se-radio.net/2024/09/se-radio-632-goran-petrovic-on-mutation-testing-at-google/]SE Radio Episode 632[/link]"
+)
+podcast_tree.add(
+    "[link=https://se-radio.net/2024/07/se-radio-625-jonathan-schneider-on-automated-refactoring-with-openrewrite/]SE Radio Episode 625[/link]"
+)
+podcast_tree.add(
+    "[link=https://se-radio.net/2024/07/se-radio-624-marcelo-trylesinski-on-fastapi/]SE Radio Episode 624[/link]"
+)
 
 about = """\
 \nHello, my name is [link=https://www.gregorykapfhammer.com]Gregory M. Kapfhammer![/link] I'm a faculty member in the Department of Computer and Information Science at Allegheny College. I'm a professor, researcher, engineer, and podcaster.
@@ -47,9 +69,11 @@ console.print(Columns([panel]))
 console.print()
 console.print(Columns([teaching_tree, research_tree], width=30))
 console.print()
-console.print(Columns([software_tree, teaching_tree], width=30))
+console.print(Columns([software_tree, podcast_tree], width=30))
 console.print()
-console.print("🚧 Run this command to generate this file: `uv run --with rich generate-readme.py`")
+console.print(
+    "🚧 Run this command to generate the README.md file: uv run --with rich generate-readme.py"
+)
 
 CONSOLE_HTML_FORMAT = """\
 <pre style="font-family:Menlo,'DejaVu Sans Mono',consolas,'Courier New',monospace">{code}</pre>
